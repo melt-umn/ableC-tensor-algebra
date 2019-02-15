bcfc630749a4b787e58e8b4aca59c90973b28ce6
Lucas Kramer <krame505@umn.edu>
2019-02-15 13:38:28 -0600

    Remove now-uneeded module attribute that was causing GCC warnings

--- grammars/edu.umn.cs.melt.exts.ableC.tensorAlgebra/abstractsyntax/decls/DeclStruct.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensorAlgebra/abstractsyntax/decls/DeclStruct.sv
@@ -11,7 +11,7 @@ Decl ::= fmt::TensorFormat
     maybeTagDecl(
       s"tensor_${fmtNm}",
       ableC_Decl {
-        struct __attribute__((refId($stringLiteralExpr{s"edu:umn:cs:melt:exts:ableC:tensorAlgebra:tensor_${fmtNm}"}), module("edu:umn:cs:melt:exts:ableC:tensorAlgebra:tensor"))) $name{s"tensor_${fmtNm}"} {
+        struct __attribute__((refId($stringLiteralExpr{s"edu:umn:cs:melt:exts:ableC:tensorAlgebra:tensor_${fmtNm}"}))) $name{s"tensor_${fmtNm}"} {
           unsigned long* dims;
           unsigned long*** indices;
           double* data;
